07d6b4c2997950c29af2ba81beb95798a4571293
Alex Carr <alexcarr1113@outlook.com>
2026-02-04 13:21:29 +0000

    Add handling of up to 10 arguments

--- main.c
+++ main.c
@@ -9,9 +9,21 @@
 #include <string.h>
 
 #define MAX_INPUT_LENGTH 256
+#define MAX_ARGUMENTS 10
 
-void execute(char* program) {
-    char *argv[] = {program, NULL};
+void execute(char* input) {
+    char* program = strtok(input, " ");
+    char *argv[MAX_ARGUMENTS] = {NULL};
+
+    int i = 0;
+    argv[i] = program;
+
+    char* nextArg;
+    do {
+        nextArg = strtok(NULL, " ");
+        argv[++i] = nextArg;
+    } while (nextArg != NULL);
+
     if (execvp(program, argv) == -1) {
         printf("Error starting %s: %s\n", argv[0], strerror(errno));
     }
@@ -24,7 +36,8 @@ int main() {
         printf("> ");
 
         char* input = malloc(MAX_INPUT_LENGTH);
-        scanf("%s", input);
+        fgets(input, MAX_INPUT_LENGTH, stdin);
+        input[strlen(input) - 1] = '\0'; // Strip the newline character
 
         if (strcmp(input, "exit") == 0) return 0;
 
